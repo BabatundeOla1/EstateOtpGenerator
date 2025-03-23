0101a21d88e8ea7981c18d76e5fd83cdde92f19d
BabatundeOla1 <Olaleyebabatun@gmail.com>
2025-03-23 21:17:11 +0100

    final Update

--- src/main/java/com/theezy/exception/ExceptionHandlers.java
+++ src/main/java/com/theezy/exception/ExceptionHandlers.java
@@ -8,24 +8,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionHandlers {
     @ExceptionHandler(IllegalArgumentException.class)
-    public String handleIllegalArgumentException(IllegalArgumentException e){
-        return e.getMessage();
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e){
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(UserAlreadyExistException.class)
     public ResponseEntity<ErrorResponse> handleUserAlreadyExistException(UserAlreadyExistException e){
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
-//        return e.getMessage();
     }
 
     @ExceptionHandler(VisitorNotFoundException.class)
-    public String handleVisitorNotFoundException(VisitorNotFoundException e){
-        return e.getMessage();
+    public ResponseEntity<ErrorResponse> handleVisitorNotFoundException(VisitorNotFoundException e){
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(OtpExpiredException.class)
-    public String handleOtpExpiredException(OtpExpiredException e){
-        return e.getMessage();
+    public ResponseEntity<ErrorResponse> handleOtpExpiredException(OtpExpiredException e){
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 }
